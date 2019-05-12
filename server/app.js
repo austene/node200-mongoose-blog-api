12ebb545dffdd901c36789ae1aed47b5c7a8d4f1
AE <austence@sbcglobal.net>
2019-05-11 18:58:11 -0700

    updates app.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,8 @@ require('dotenv').config();
 const MONGODB_URI = process.env.MONGODB_URI
 console.log(MONGODB_URI)
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect('mongodb://heroku_8jsx5fck:ag05h99lksf9t0dqokmolp5b3k@ds155596.mlab.com:55596/heroku_8jsx5fck', { useNewUrlParser: true });
 mongoose.Promise = Promise;
 
 const app = express();
